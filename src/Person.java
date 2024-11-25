09fc5f4162625b28b803f5cdd6bc416e6e081e73
edulanarf <edulanarfrgmail.com>
2024-11-25 19:08:48 +0000

    Creación de la clase Person y sus respectivos metodos

---
+++ src/Person.java
@@ -0,0 +1,33 @@
+
+import java.time.LocalDate;
+import java.util.StringJoiner;
+
+public class Person {
+    private final String name;
+    private final LocalDate birthday;
+
+    public Person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public int age(){
+        return LocalDate.now().getYear() - birthday.getYear();
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Person.class.getSimpleName() + "[", "]")
+                .add("name='" + name + "'")
+                .add("birthday=" + birthday)
+                .toString();
+    }
+}
